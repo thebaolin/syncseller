4095b63eb3dc24a5021600e0784af0b075f07d42
RandyLu0 <33765989+RandyLu0@users.noreply.github.com>
2025-03-22 15:34:07 -0400

    ebay oauth visible

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,7 +3,7 @@ import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 import { createListing } from './ebay'
-import { app, BrowserWindow, ipcMain} from 'electron/main'
+import { app, BrowserWindow, ipcMain } from 'electron/main'
 import crypto from 'crypto'
 import EbayAuthToken from 'ebay-oauth-nodejs-client'
 import { request } from 'node:https'
@@ -178,6 +178,130 @@ app.whenReady().then(() => {
     })
 
     // EBAY
+
+    const win = new BrowserWindow({
+        width: 400,
+        height: 400,
+        webPreferences: { nodeIntegration: false, contextIsolation: true }
+    })
+
+    // hardcoded, should have values extracted from database; entered from user
+    const ebayAuthToken = new EbayAuthToken({
+        clientId: 'RandyLu-sand-SBX-e41907e53-a28e5f11',
+        clientSecret: 'SBX-41907e53e228-9004-4bbf-81f5-a63c',
+        redirectUri: 'Randy_Lu-RandyLu-sand-SB-domszhbw',
+        env: 'SANDBOX'
+    })
+    // oauth scopes for what api calls you can make
+    const scopes = [
+        'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.inventory'
+    ]
+    const oauth_url = ebayAuthToken.generateUserAuthorizationUrl('SANDBOX', scopes, {
+        prompt: 'login'
+    })
+
+    // gets an authorization token for the user
+    win.loadURL(oauth_url).then(() => {
+        win.webContents.addListener('did-redirect-navigation', async (details) => {
+            const access_code = new URL(details.url).searchParams.get('code')
+            if (access_code) {
+                const accessToken = await ebayAuthToken.exchangeCodeForAccessToken(
+                    'SANDBOX',
+                    access_code
+                )
+                console.log(accessToken)
+                const auth = JSON.parse(accessToken).access_token
+                console.log(auth)
+
+                // extracted and populated from db entry x
+                const data = `{
+    "product": {
+        "title": "Test listing - do not bid or buy - awesome Apple watch test 2",
+        "aspects": {
+            "Feature":[
+              "Water resistance", "GPS"
+            ],
+            "CPU":[
+              "Dual-Core Processor"
+            ]
+        },
+        "description": "Test listing - do not bid or buy. Built-in GPS. Water resistance to 50 meters.1 A new lightning-fast dual-core processor. And a display that is two times brighter than before. Full of features that help you stay active, motivated, and connected, Apple Watch Series 2 is designed for all the ways you move ",
+        "upc": ["888462079525"],
+        "imageUrls": [
+            "http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/S/1/S1/42/S1-42-alu-silver-sport-white-grid?wid=332&hei=392&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1472247758975",
+            "http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/4/2/42/stainless/42-stainless-sport-white-grid?wid=332&hei=392&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1472247760390",
+            "http://store.storeimages.cdn-apple.com/4973/as-images.apple.com/is/image/AppleInc/aos/published/images/4/2/42/ceramic/42-ceramic-sport-cloud-grid?wid=332&hei=392&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=1472247758007"
+        ]
+    },
+    "condition": "NEW",
+    "packageWeightAndSize": {
+        "dimensions": {
+            "height": 5,
+            "length": 10,
+            "width": 15,
+            "unit": "INCH"
+        },
+        "packageType": "MAILING_BOX",
+        "weight": {
+            "value": 2,
+            "unit": "POUND"
+        }
+    },
+    "availability": {
+        "shipToLocationAvailability": {
+            "quantity": 10
+        }
+    }
+}`
+
+                // Define the options for the HTTPS request
+                const options = {
+                    hostname: 'api.sandbox.ebay.com',
+                    path: 'https://api.sandbox.ebay.com/sell/inventory/v1/inventory_item/50',
+                    method: 'PUT',
+                    headers: {
+                        Authorization: 'Bearer ' + auth,
+                        Accept: 'application/json',
+                        'Content-Length': data.length,
+                        'Content-Type': 'application/json',
+                        'Content-Language': 'en-US'
+                    }
+                }
+
+                // REPLACE WITH RESPONSE HANDLING CODE
+                // Create the request
+                const req = request(options, (res) => {
+                    let responseBody = ''
+
+                    // Listen for data from the response
+                    res.on('data', (chunk) => {
+                        responseBody += chunk
+                    })
+
+                    // Listen for the end of the response
+                    res.on('end', () => {
+                        console.log('Response:', responseBody)
+                    })
+                })
+
+                // Handle any errors with the request
+                req.on('error', (e) => {
+                    console.error(`Problem with request: ${e.message}`)
+                })
+
+                // Write data to the request body
+                req.write(data)
+
+                // End the request
+                req.end()
+
+                win.loadURL('https://api.sandbox.ebay.com/sell/inventory/v1/inventory_item/50')
+
+                // store into the db somehow
+            }
+        })
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
