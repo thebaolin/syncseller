c4d912a5f6a61c4fa274fd6207cdccde4e9c2045
RandyLu0 <33765989+RandyLu0@users.noreply.github.com>
2025-05-13 10:44:50 -0400

    upload multiple images support, began work on posting to ebay

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,7 +5,7 @@ import icon from '../../resources/icon.png?asset'
 import { app, BrowserWindow, ipcMain } from 'electron/main'
 import EbayAuthToken from 'ebay-oauth-nodejs-client'
 import { request } from 'node:https'
-import { ebay_oauth_flow, post_image, post_listing } from './ebay'
+import { create_inventory_item, ebay_oauth_flow, post_listing, refresh } from './ebay'
 import { createDummyShopifyListing } from './shopify'
 import { setupEtsyOAuthHandlers } from './etsy'
 
@@ -175,7 +175,8 @@ app.whenReady().then(() => {
 
     setupEtsyOAuthHandlers()
 
-    post_listing('t.jpeg')
+    create_inventory_item()
+    //refresh()
 
     app.on('activate', function () {
         // On macOS it's common to re-create a window in the app when the
